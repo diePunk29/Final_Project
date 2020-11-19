bd1cdd376e84ab32af80117ee405def94222c6ac
cristian mosqueda <cmosqued@gmail.com>
2020-11-18 20:39:47 -0700

    Just added the constructor and inheritance part mates.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-public class Main {
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public Main() {}
 
     public static void main(String[] args) {
 	// write your code here
