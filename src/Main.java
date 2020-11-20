a13330e12e6b7c2b85ac4c1afc9e0a1e3afcd0a2
cristian mosqueda <cmosqued@gmail.com>
2020-11-20 14:47:19 -0700

    Basic setup of JMenu.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,28 @@ import java.awt.*;
 
 public class Main extends JFrame {
 
-    public Main() {}
+    public Main() {
+        Controller co = new Controller();
+        // I think the main menu is suppose to be set up here idk tho
+        JMenuBar mainBar = new JMenuBar();
+        setJMenuBar(mainBar);
+
+        // file and about menus
+        JMenu fileMenu = new JMenu("File");
+        JMenu aboutMenu = new JMenu("About");
+
+        mainBar.add(fileMenu);
+        mainBar.add(aboutMenu);
+
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Main daMain = new Main();
+        daMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        daMain.setVisible(true);
+        daMain.setSize(800, 600);
+
+
     }
 }
