e2917fafd354a39c9dff5a9477c04271a0ad9b6b
cristian mosqueda <cmosqued@gmail.com>
2020-11-20 14:18:03 -0700

    Added view class from view portion of the project design.

---
+++ src/View.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.util.Observer;
+import java.util.Observable;
+
+public class View extends JPanel implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
