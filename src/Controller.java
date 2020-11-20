838a6ab9c5c9314609f9d9502d3e07af60f10f77
cristian mosqueda <cmosqued@gmail.com>
2020-11-20 14:29:28 -0700

    Controller class.

---
+++ src/Controller.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Controller implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
