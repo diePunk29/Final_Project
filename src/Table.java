5475c8cfc5586f46dd4e5429134f940550fc482a
thefakemacaw <38474041+thefakemacaw@users.noreply.github.com>
2020-11-21 14:21:11 -0700

    base Jtable

---
+++ src/Table.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Table {
+    // the code below is from https://www.javatpoint.com/java-jtable
+    JFrame f;
+
+    Table() {
+        f = new JFrame();
+        String[][] data = {{"101", "Amit", "670000"},
+                {"102", "Jai", "780000"},
+                {"101", "Sachin", "700000"}};
+        String[] column = {"ID", "NAME", "SALARY"};
+        JTable jt = new JTable(data, column);
+        jt.setBounds(30, 40, 200, 300);
+        JScrollPane sp = new JScrollPane(jt);
+        f.add(sp);
+        f.setSize(300, 400);
+        f.setVisible(true);
+    }
+}
