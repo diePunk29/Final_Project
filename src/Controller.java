3b00de59a08fb8556c71fba7bfa43adbaa0f893e
cristian mosqueda <cmosqued@gmail.com>
2020-11-23 20:54:50 -0700

    Defined action listener components in Controller class.

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,44 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
 public class Controller implements ActionListener {
+
+    // fields
+    JMenu fileMenu;
+    JMenu aboutMenu;
+    JOptionPane dia;
+    JMenuItem loadRost;
+    JMenuItem addAttendance;
+    JMenuItem save;
+    JMenuItem plotData;
+
+    // constructor + methods
+    public Controller() {
+        fileMenu = new JMenu("File");
+        aboutMenu = new JMenu("About");
+
+        dia = new JOptionPane();
+        dia.setMessage(" TEAM NAME: DA AMAZING FELLAS!\n" +
+                " TEAM MEMBERS: Cristian Mosqueda, Albert Schaeffer, Tyler Vaillancourt," +
+                "Will Lord, I forgot the rest lol.\n");
+        aboutMenu.add(dia);
+
+        // JMenu items for File menu component
+        loadRost = new JMenuItem("Load a Roster");
+        addAttendance = new JMenuItem("Add Attendance");
+        save = new JMenuItem("Save");
+        plotData = new JMenuItem("Plot Data");
+
+        // adding menu items to file menu
+        fileMenu.add(loadRost);
+        fileMenu.add(addAttendance);
+        fileMenu.add(save);
+        fileMenu.add(plotData);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        System.out.println();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,13 @@ import java.awt.*;
 public class Main extends JFrame {
 
     public Main() {
-        Controller co = new Controller();
         // I think the main menu is suppose to be set up here idk tho
         JMenuBar mainBar = new JMenuBar();
         setJMenuBar(mainBar);
+        Controller co = new Controller();
 
-        // file and about menus components
-        JMenu fileMenu = new JMenu("File");
-        JMenu aboutMenu = new JMenu("About");
-
-        mainBar.add(fileMenu);
-        mainBar.add(aboutMenu);
-
-        JOptionPane dia = new JOptionPane();
-        dia.setMessage(" TEAM NAME: DA AMAZING FELLAS!\n" +
-                " TEAM MEMBERS: Cristian Mosqueda, Albert Schaeffer, Tyler Vaillancourt," +
-                "Will Lord, I forgot the rest lol.\n");
-        aboutMenu.add(dia);
-
-        // JMenu items for File menu component
-        JMenuItem loadRost = new JMenuItem("Load a Roster");
-        JMenuItem addAttendance = new JMenuItem("Add Attendance");
-        JMenuItem save = new JMenuItem("Save");
-        JMenuItem plotData = new JMenuItem("Plot Data");
-
-        // adding menu items to file menu
-        fileMenu.add(loadRost);
-        fileMenu.add(addAttendance);
-        fileMenu.add(save);
-        fileMenu.add(plotData);
-
-
-
-
-
+        mainBar.add(co.fileMenu);
+        mainBar.add(co.aboutMenu);
     }
 
     public static void main(String[] args) {
